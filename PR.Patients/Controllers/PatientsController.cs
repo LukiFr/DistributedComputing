f9ae30052ee09676c69f2c72da1340ba05a46101
Łukasz Franiewski <lukifraniewski@gmail.com>
2020-10-11 22:56:44 +0200

    Working app from lecture 1.

---
+++ PR.Patients/Controllers/PatientsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DistributedComputing.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DistributedComputing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        public readonly PrDataContext _context;
+
+        public PatientsController(PrDataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Patients.ToList());
+        }
+
+
+    }
+}
---
+++ PR.Patients/Model/Patient.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistributedComputing.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public System.DateTime DateOfPositiveTest { get; set; }
+
+    }
+}
